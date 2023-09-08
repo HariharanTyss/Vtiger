6314fb7e0789e94430c2e09fd596168e34b56be2
HP ELITEBOOK <HP ELITEBOOK@Hariharan>
2023-09-08 18:12:24 +0530

    no Changes

--- CRM-VTIGER/src/test/java/demo/DemoTest2.java
+++ CRM-VTIGER/src/test/java/demo/DemoTest2.java
@@ -16,21 +16,24 @@ public class DemoTest2
 	@Test
 	public void demotest2() throws InterruptedException
 	{
-		String url = System.getProperty("url");
-		String unm = System.getProperty("unm");
-		String pwd = System.getProperty("pwd");
+		String url = "https://www.shoppersstack.com/user-signin";
+		//String unm = "unm";
+		//String pwd="pwd";
+		
 		
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get(url);
+		driver.findElement(By.xpath("//span[contains(text(),'Login')]")).click();
 		
-		driver.findElement(By.name("user_name")).sendKeys(unm);
-		Thread.sleep(1000);
-		driver.findElement(By.name("user_password")).sendKeys(pwd);
-		Thread.sleep(1000);
-		driver.findElement(By.id("submitButton")).click();
-		driver.quit();
+//		driver.findElement(By.name("user_name")).sendKeys(unm);
+//		Thread.sleep(1000);
+//		driver.findElement(By.name("user_password")).sendKeys(pwd);
+//		Thread.sleep(1000); 
+//		driver.findElement(By.id("submitButton")).click();
+		
+		//driver.quit();
 		
 	}
 
